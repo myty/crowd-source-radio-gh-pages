bec346a903ee640ce377bf55385bdae373ad83e6
Michael Tyson <michaeltyson@outlook.com>
2019-12-30 14:06:21 -0500

    Release v0.1.0-beta1

---
+++ precache-manifest.abfbf30fe56f3f0efbc5dfbf9a92c7e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084728c1155a48ec05672f50ed836f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "765fc4d51c1fad34fa35",
+    "url": "/static/css/main.f777d276.chunk.css"
+  },
+  {
+    "revision": "b8515e0ed1b5af5ffbfd",
+    "url": "/static/js/2.937e7aed.chunk.js"
+  },
+  {
+    "revision": "765fc4d51c1fad34fa35",
+    "url": "/static/js/main.cafcf070.chunk.js"
+  },
+  {
+    "revision": "5302a05d63b96d48db6c",
+    "url": "/static/js/runtime-main.db3500ee.js"
+  }
+]);
